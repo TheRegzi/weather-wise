432f938dbdcbe8dd619715896259711408ea8ad2
Regine Kornbakk <reginekornbakk@gmail.com>
2025-06-16 15:14:24 +0200

    feat: optimized style for mobile view

--- src/app/searchresults/page.tsx
+++ src/app/searchresults/page.tsx
@@ -44,22 +44,22 @@ export default function SearchResultsPage() {
   if (!search) return <div>No search term</div>;
 
   return (
-    <div className="flex flex-col justify-center mx-auto p-8 w-[1000px]">
+    <div className="flex flex-col justify-center mx-auto p-5 md:p-8 w-full lg:w-[1000px]">
       <h1 className="text-left font-semibold text-4xl font-inter mt-5 text-shadow">
         Search results
       </h1>
       <p className="font-roboto italic font-light text-xl my-2 text-shadow">
         Showing results for &quot;{search}&quot;:
       </p>
-      <div className="bg-background-secondary py-1 px-10 mt-3">
+      <div className="bg-background-secondary py-1 px-2 md:px-10 mt-3">
         {places.length > 0 ? (
           <ul className="list-disc mt-3">
             {places.map((place) => (
               <li
                 key={place.id || `${place.name}-${place.latitude}-${place.longitude}`}
-                className="text-lg flex items-center gap-1 border-b pb-1 my-4 text-xl font-display text-shadow"
+                className="text-lg flex items-center gap-2 border-b pb-1 my-4 text-xl font-display text-shadow"
               >
-                <FontAwesomeIcon icon={faLocationDot} className="text-footer mr-2" />
+                <FontAwesomeIcon icon={faLocationDot} className="text-footer" />
                 <Link
                   href={`/single-place-weather/${place.latitude}/${place.longitude}?name=${encodeURIComponent(place.name)}`}
                   className="font-semibold hover:underline transition"
