b408578df1917bc3606796fd464aa85d06c83a14
Regine Kornbakk <reginekornbakk@gmail.com>
2025-06-25 14:38:49 +0200

    feat: adding content for about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function About() {
   return (
-    <main>
-      <h1 className="">About</h1>
+    <main className="w-[1000px] mx-auto flex flex-col">
+      <Image
+        src="/assets/logo.png"
+        alt="Logo"
+        width={150}
+        height={150}
+        className="w-[150px] h-[150px]"
+      />
+      <h1 className="font-inter font-semibold">About us</h1>
     </main>
   );
 }
