7399a87a17027f1ccfcc52d95b2acce93bd38081
Regine Kornbakk <reginekornbakk@gmail.com>
2025-06-26 15:57:17 +0200

    feat: adding content for contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,9 @@
 export default function Contact() {
   return (
-    <main>
-      <h1>Contact page</h1>
+    <main className="w-[1000px] flex flex-col mx-auto justify-center my-16">
+      <h1 className="font-inter font-semibold text-3xl text-shadow">Contact us</h1>
+      <div className="bg-background-secondary p-6 mt-6"></div>
+      <div></div>
     </main>
   );
 }
